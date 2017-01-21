97d61de66868e869caa5f1171932c5be0c7b47b9
Yixin He <Yixin He>
2017-01-21 14:52:58 -0500

    first person camera

--- main.py
+++ main.py
@@ -9,91 +9,92 @@ from direct.interval.IntervalGlobal import Sequence
 from panda3d.core import Point3
  
 class MyApp(ShowBase):
-    def __init__(self):
-        ShowBase.__init__(self)
+	def __init__(self):
+		ShowBase.__init__(self)
+		self.x,self.y,self.angleDegrees,self.angleRadians =0,0,90,pi/2
+		# Disable the camera trackball controls.
+		# self.disableMouse()
  
-        # Disable the camera trackball controls.
-        # self.disableMouse()
- 
-        # Load the environment model.
-        self.scene = self.loader.loadModel("land/1.egg")
-        # Reparent the model to render.
-        self.scene.reparentTo(self.render)
-        # Apply scale and position transforms on the model.
-        self.scene.setScale(1, 1, 1)
-        self.scene.setPos(-8, 42, 0)
-        self.scene.setColor(.95, .85, .9, 1.0)
+		# Load the environment model.
+		self.scene = self.loader.loadModel("land/1.egg")
+		# Reparent the model to render.
+		self.scene.reparentTo(self.render)
+		# Apply scale and position transforms on the model.
+		self.scene.setScale(1, 1, 1)
+		self.scene.setPos(-8, 42, 0)
+		self.scene.setColor(.95, .85, .9, 1.0)
 
-        tex = loader.loadTexture('land/5.jpg')
-        self.scene.setTexGen(TextureStage.getDefault(), TexGenAttrib.MWorldPosition)
-        self.scene.setTexture(tex)
+		tex = loader.loadTexture('land/5.jpg')
+		self.scene.setTexGen(TextureStage.getDefault(), TexGenAttrib.MWorldPosition)
+		self.scene.setTexture(tex)
 
 
-        # # Use a 512x512 resolution shadow map
-        # plight.setShadowCaster(True, 512, 512)
-        # # Enable the shader generator for the receiving nodes
-        # self.render.setShaderAuto()
+		# # Use a 512x512 resolution shadow map
+		# plight.setShadowCaster(True, 512, 512)
+		# # Enable the shader generator for the receiving nodes
+		# self.render.setShaderAuto()
  
-        # Add the spinCameraTask procedure to the task manager.
-        # self.taskMgr.add(self.spinCameraTask, "SpinCameraTask")
+		# Add the spinCameraTask procedure to the task manager.
+		self.taskMgr.add(self.moveCameraTask, "moveCameraTask")
  
-        # Load and transform the panda actor.
-        self.basicActor = Actor("person.egg",
-                                {"walk": "person-Anim1.egg"})
-        # Reparent the model to render.
-        self.basicActor.reparentTo(self.render)
-        # Apply scale and position transforms on the model.
-        self.basicActor.setScale(1, 1, 1)
-        self.basicActor.setPos(-8, 42, 0)
-        self.basicActor.setColorScale(.95, .92, .99, 1.0)
+		# Load and transform the panda actor.
+		self.basicActor = Actor("person.egg",
+								{"walk": "person-Anim1.egg"})
+		# Reparent the model to render.
+		self.basicActor.reparentTo(self.render)
+		# Apply scale and position transforms on the model.
+		self.basicActor.setScale(1, 1, 1)
+		self.basicActor.setPos(-8, 42, 0)
+		self.basicActor.setColorScale(.95, .92, .99, 1.0)
 
-        tex = loader.loadTexture('land/16.jpg')
-        self.basicActor.setTexGen(TextureStage.getDefault(), TexGenAttrib.MWorldPosition)
-        self.basicActor.setTexture(tex)
+		tex = loader.loadTexture('land/16.jpg')
+		self.basicActor.setTexGen(TextureStage.getDefault(), TexGenAttrib.MWorldPosition)
+		self.basicActor.setTexture(tex)
 
-        # Loop its animation.
-        self.basicActor.loop("walk")
+		# Loop its animation.
+		self.basicActor.loop("walk")
  
-        # # Create the four lerp intervals needed for the panda to
-        # # walk back and forth.
-        # pandaPosInterval1 = self.pandaActor.posInterval(13,
-        #                                                 Point3(0, -10, 0),
-        #                                                 startPos=Point3(0, 10, 0))
-        # pandaPosInterval2 = self.pandaActor.posInterval(13,
-        #                                                 Point3(0, 10, 0),
-        #                                                 startPos=Point3(0, -10, 0))
-        # pandaHprInterval1 = self.pandaActor.hprInterval(3,
-        #                                                 Point3(180, 0, 0),
-        #                                                 startHpr=Point3(0, 0, 0))
-        # pandaHprInterval2 = self.pandaActor.hprInterval(3,
-        #                                                 Point3(0, 0, 0),
-        #                                                 startHpr=Point3(180, 0, 0))
- 
-        # # Create and play the sequence that coordinates the intervals.
-        # self.pandaPace = Sequence(pandaPosInterval1,
-        #                           pandaHprInterval1,
-        #                           pandaPosInterval2,
-        #                           pandaHprInterval2,
-        #                           name="pandaPace")
-        # self.pandaPace.loop()
-        ambientLight = AmbientLight('ambientLight')
-        ambientLight.setColor(Vec4(0.6, 0.6, 0.6, 1))
-        ambientLightNP = render.attachNewNode(ambientLight)
-        self.render.setLight(ambientLightNP)
+		
+		ambientLight = AmbientLight('ambientLight')
+		ambientLight.setColor(Vec4(0.6, 0.6, 0.6, 1))
+		ambientLightNP = render.attachNewNode(ambientLight)
+		self.render.setLight(ambientLightNP)
 
-        plight = PointLight('plight')
-        plight.setColor(VBase4(0.6, 0.6, 0.6, 1))
-        plnp = render.attachNewNode(plight)
-        plnp.setPos(10, 20, 0)
-        self.render.setLight(plnp)
- 
-    # Define a procedure to move the camera.
-    def spinCameraTask(self, task):
-        angleDegrees = task.time * 6.0
-        angleRadians = angleDegrees * (pi / 180.0)
-        self.camera.setPos(20 * sin(angleRadians), -20.0 * cos(angleRadians), 3)
-        self.camera.setHpr(angleDegrees, 0, 0)
-        return Task.cont
- 
+		plight = PointLight('plight')
+		plight.setColor(VBase4(0.6, 0.6, 0.6, 1))
+		plnp = render.attachNewNode(plight)
+		plnp.setPos(10, 20, 0)
+		self.render.setLight(plnp)
+
+		self.keyMap = {"w" : False, "s" : False, "a" : False, "d" : False,}
+		self.accept("w", self.setKey, ["w", True])
+		self.accept("s", self.setKey, ["s", True])   
+		self.accept("a", self.setKey, ["a", True])   
+		self.accept("d", self.setKey, ["d", True])
+
+		self.accept("w-up", self.setKey, ["w", False])
+		self.accept("s-up", self.setKey, ["s", False])
+		self.accept("a-up", self.setKey, ["a", False])
+		self.accept("d-up", self.setKey, ["d", False])
+
+	def setKey(self, key, value):
+	  self.keyMap[key] = value
+   
+	# Define a procedure to move the camera.
+	def moveCameraTask(self, task):
+		if(self.keyMap["w"] == True):
+			self.y+=1
+		elif(self.keyMap["s"] == True):
+			self.y-=1
+		elif(self.keyMap["a"] == True):
+			self.angleDegrees+=1
+			self.angleRadians+=2*pi/360
+		elif(self.keyMap["d"] == True):
+			self.angleDegrees-=1
+			self.angleRadians-=2*pi/360
+		self.camera.setPos(self.y*cos(self.angleRadians), self.y*sin(self.angleRadians),3)
+		self.camera.setHpr(self.angleDegrees-90, 0, 0)
+		return task.cont
+
 app = MyApp()
 app.run()
